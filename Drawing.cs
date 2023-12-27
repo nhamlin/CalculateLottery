95b34205ad3f8506c2aa6096273098a75139d6db
Nick Hamlin <nicholas_hamlin@hotmail.com>
2023-12-27 01:34:00 +0000

    Added models

---
+++ Drawing.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+public class Drawing {
+    public Drawing () {
+
+    }
+    public DateTime DrawingDate {get;set;}
+    public int[] Numbers { get;set;}
+    public int BigNumber {get;set;}
+    public Game GameName {get;set;}
+    public int Multiplier {get;set;}
+    public double Jackpot {get;set;}
+
+}
---
+++ Payout.cs
@@ -0,0 +1,5 @@
+public class Payout {
+    public int WhiteBallMatches {get;set;}
+    public bool BigNumberMatch {get;set;}
+    public decimal Amount {get;set;}
+}
